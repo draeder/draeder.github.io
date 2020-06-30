d3ae3ff81d1daa89778c5aed70d0375d4d4aae82
draeder <draeder@gmail.com>
2020-06-30 18:09:25 -0500

    Update index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+////UI
+let myScript = document.createElement("script");
+myScript.setAttribute("src", "https://www.example.com/foo.js");
+document.body.appendChild(myScript);
+
+
+////Backend
+document.addEventListener('DOMContentLoaded', async () => {
+    let myScript = document.createElement("p");
+    p.setAttribute("innerText", "https://www.example.com/foo.js");
+    document.body.appendChild(p);
+
+})
