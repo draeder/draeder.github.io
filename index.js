7396a52520569f219efee0125005ce1ca32cf736
draeder <draeder@gmail.com>
2020-06-30 18:30:29 -0500

    .

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 document.addEventListener('DOMContentLoaded', async () => {
+    //UI Handler
+    
+    //Bugout
+    //IPFS
+    //AvionDB
 
-    ////UI
-    let myScript = document.createElement("script");
-    myScript.setAttribute("src", "https://www.example.com/foo.js");
-    document.body.appendChild(myScript);
 
 
-    ////Backend
-
-    let myScript = document.createElement("p");
-    p.setAttribute("innerText", "https://www.example.com/foo.js");
-    document.body.appendChild(p);
 
 })
